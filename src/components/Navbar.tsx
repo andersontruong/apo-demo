9a6f407aca565c4f8ea830762e70be4deaeef5c4
anderson-truong <andersonleetruong@gmail.com>
2023-12-09 23:07:46 -0800

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,14 +27,14 @@ export default function Navbar() {
       
     return (
     <>
-    <header className={`flex w-full h-[80px] sticky top-0 items-center py-6 px-10 lg:px-20  justify-between z-10 transition ease-in-out delay-50 duration-300 bg-white ${shadow ? 'shadow-xl' : ''}`}>
+    <header className={`flex w-full h-[70px] sticky top-0 items-center py-6 px-10 lg:px-20  justify-between z-10 transition ease-in-out delay-50 duration-300 bg-white ${shadow ? 'shadow-xl' : ''}`}>
         <Link className="group flex items-center space-x-2"  href="/">
           <img className="w-8" src="/Alpha_Phi_Omega.png" />
           <div className="flex flex-col items-center -space-y-2">
-            <h1 className={`drop-shadow-xl text-sm md:text-lg text-blue-800 font-medium transition ease-in-out delay-50 duration-300 group-hover:text-blue-300 ${abril.className}`}>
+            <h1 className={`drop-shadow-xl text-sm md:text-base text-black font-medium transition ease-in-out delay-50 duration-300 group-hover:text-neutral-500 ${abril.className}`}>
               Alpha Phi Omega
             </h1>
-            <h1 className={`drop-shadow-xl text-xs md:text-base text-yellow-700 font-medium transition ease-in-out delay-50 duration-300 group-hover:text-yellow-500 ${abril.className}`}>
+            <h1 className={`drop-shadow-xl text-xs md:text-sm text-black font-medium transition ease-in-out delay-50 duration-300 group-hover:text-neutral-500 ${abril.className}`}>
               Chi Chapter
             </h1>
           </div>
@@ -56,7 +56,7 @@ export default function Navbar() {
           <Link href="/" className="mt-1 transition ease-in-out delay-50 duration-200 border-b-2 border-transparent hover:border-black">
             Contact Us
           </Link>
-          <button onClick={() => {setLoginEnabled(!loginEnabled)}} className="hover:text-white px-8 py-2 bg-blue-100 rounded-md hover:bg-blue-600 transition ease-in-out delay-50 duration-200">
+          <button onClick={() => {setLoginEnabled(!loginEnabled)}} className="text-blue-500 hover:text-white font-medium px-4 py-1 bg-blue-100 rounded-sm hover:bg-blue-400 transition ease-in-out delay-50 duration-200">
             Login
           </button>
         </div>
