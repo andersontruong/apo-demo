c4a2eb8b17f185fa4b39c14abaaad84618880d94
anderson-truong <andersonleetruong@gmail.com>
2024-01-24 15:24:35 -0800

    event form padding

--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -3,7 +3,7 @@ import EventForm from "./EventForm";
 export default function NewEventPage() {
     
     return <main className="flex flex-col justify-center md:flex-row w-full min-h-screen">
-        <div className="my-10 h-full flex flex-col justify-center space-y-10">
+        <div className="my-10 h-full flex flex-col justify-center space-y-10 px-4">
             <h1 className="text-4xl text-center">Create Event - Sample</h1>
             <EventForm />
         </div>
